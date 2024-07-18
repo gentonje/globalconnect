1209c3a724d6ca440d3fef00b7c2d8725258c844
muchai <mngash2005@gmail.com>
2024-07-18 16:31:35 +0300

    product images

--- components/global/ProductDetail.tsx
+++ components/global/ProductDetail.tsx
@@ -4,16 +4,18 @@ import { ChevronLeft } from 'lucide-react';
 import { ProductDetailProps } from '@/types/globalShopping';
 
 export const ProductDetail: React.FC<ProductDetailProps> = ({ product, onBack, onAddToCart }) => (
-  <div className="p-4">
+  <div className="p-4 ">
     <button onClick={onBack} className="mb-4 flex items-center text-purple-400">
       <ChevronLeft size={16} />
       <span className="ml-1 text-sm">Back</span>
     </button>
-    <div className="bg-gray-800 p-4 rounded">
-   
-      <h2 className="text-xl font-bold text-center mb-2">{product.name}</h2>
-      <p className="text-center text-green-400 text-lg mb-3">${product.price.toFixed(2)}</p>
-      <p className="text-gray-300 mb-4 text-sm">{product.description}</p>
+    <div className="bg-white p-4 rounded-lg">
+    <div className=" flex items-center justify-center">       
+        <img src={product.imageUrl} alt={product.name} className="object-contain rounded w-40 h-40" />
+      </div>
+      <h2 className="text-xl text-teal-950 font-bold text-center mb-2">{product.name}</h2>
+      <p className="text-center text-red-800 text-lg mb-3 font-semibold">Kshs {product.price.toFixed(0)}</p>
+      <p className="text-teal-700 mb-4 text-sm text-center items-center">{product.description}</p>
       <button
         className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded text-sm"
         onClick={() => onAddToCart(product)}
